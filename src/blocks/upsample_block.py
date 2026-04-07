125dd6097be2e89251543ffa328213dcbf362b0c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-07 11:00:27 +0300

    Create upsample_block.py

---
+++ src/blocks/upsample_block.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class UpConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpConv, self).__init__()
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        return self.up(x)
